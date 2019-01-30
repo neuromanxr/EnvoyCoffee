8b389737bf2dac1f88e8ffaf0a0adc226f309355
Kelvin Lee <kelvin.lee@newsela.com>
2019-01-30 16:48:25 -0500

    Adds unit test

--- EnvoyCoffeeTests/EnvoyCoffeeTests.swift
+++ EnvoyCoffeeTests/EnvoyCoffeeTests.swift
@@ -10,18 +10,54 @@ import XCTest
 @testable import EnvoyCoffee
 
 class EnvoyCoffeeTests: XCTestCase {
+    
+    var controllerUnderTest: VenuesViewController!
+    
+    var venues = [Venue]()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        controllerUnderTest = VenueRouter.createModule()
+        controllerUnderTest.location = .envoy
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        controllerUnderTest = nil
+        super.tearDown()
     }
 
-    func testExample() {
+    func testGeneralApiCall() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let promise = expectation(description: "Promises completed")
+        
+        XCTAssertEqual(self.venues.count, 0, "should be empty before the data task runs")
+        
+        var params = [String: Any]()
+        
+        LocationManager.shared.getLatLongFromAddress().get { coordinate in
+            debugPrint("coordinate:\(coordinate)")
+            params = ["client_id": APIService.clientId, "client_secret": APIService.clientSecret, "ll": coordinate, "query": "coffee", "v": "20180323", "limit": 15]
+            }.then { _ in
+                SearchManager.shared.searchVenuesForLocation(params: params)
+            }.then { venues in
+                SearchManager.shared.setPhotoUrls(venues: venues)
+            }.get { venues in
+                self.venues = venues
+                
+                promise.fulfill()
+            }.ensure {
+                
+            }.catch { error in
+                debugPrint("searchVenuesForLocation error:\(error)")
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertEqual(self.venues.count, 15, "Didn't get expected response from call")
+        
+        XCTAssertTrue(self.venues.filter { $0.photoUrl != nil }.isEmpty, "photoUrl is nil")
     }
 
     func testPerformanceExample() {
